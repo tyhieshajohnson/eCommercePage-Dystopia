6920a6ec844dad821a9b03d72efd52f7c3333b75
tyhieshajohnson <johnsontyhiesha@gmail.com>
2023-12-08 01:04:36 +0200

    Sorting and Search Functioning

--- JS/shop.js
+++ JS/shop.js
@@ -1,46 +1,76 @@
+// define 
 let mainProduct = document.querySelector('main');
+let searchInput = document.querySelector('.searchInput');
+let priceFilter = document.querySelector('.priceFilter');
+let typeFilter = document.querySelector('.typeFilter');
+let filteredProducts = [];
 
-let product = JSON.parse(localStorage.getItem('product')) //|| [];; 
+function updateProducts() {
+    let filteredProducts = product.slice();
+    let searchTerm = searchInput.value.toLowerCase();
 
-let purchased = [];
-// purchased = JSON.parse(localStorage.getItem('purchased')) //|| [];;
+    if (searchTerm) {
+        filteredProducts = filteredProducts.filter(item =>
+            item.name.toLowerCase().includes(searchTerm) || 
+            item.description.toLowerCase().includes(searchTerm) ||
+            item.type.toLowerCase().includes(searchTerm)
+        );
+    }
 
-mainProduct.innerHTML = product.map(function(item, index) {
-    return `
-    <div class="card" style="width: 18rem;">
-    <img class="img-fluid" src='${item.url}'>
-    <h5>${item.name}</h5>
-    <p>${item.description}</p>
-    <h4 class="priceFilter">${item.price}</h4>
-    <button class="btn btn-primary" data-add value="${index}">Add To Cart</button>
-    </div>
-`
-}).join('');
+    let selectedType = typeFilter.value.toLowerCase();
+    if (selectedType !== 'all') {
+        filteredProducts = filteredProducts.filter(item => item.type.toLowerCase() === selectedType);
+    }
+
+    mainProduct.innerHTML = filteredProducts.map(function (item, index) {
+        return `
+        <div class="card" style="width: 18rem;">
+            <img class="img-fluid" src='${item.url}'>
+            <h5>${item.name}</h5>
+            <p>${item.type}</p>
+            <p>${item.description}</p>
+            <h4 class="priceFilter">${item.price}</h4>
+            <button class="btn btn-primary" data-add value="${index}">Add To Cart</button>
+        </div>
+        `;
+    }).join('');
+}
 
 function add(index) {
-    // adds new element at the ned with a new length
     purchased.push(product[index]);
-    // ccreating a new value
     localStorage.setItem('purchased', JSON.stringify(purchased));
     purchased = JSON.parse(localStorage.getItem('purchased'));
 }
 
-mainProduct.addEventListener('click', function(event) {
-    console.log('Event Listener clicked');
-    if (event.target.hasAttribute('data-add')) {
-       add(event.target.value)
+function initialize() {
+    product = JSON.parse(localStorage.getItem('product')) || [];
+    mainProduct.innerHTML = product.map(function(item, index) {
+        return `
+        <div class="card" style="width: 18rem;">
+            <img class="img-fluid" src='${item.url}'>
+            <h5>${item.name}</h5>
+            <p>${item.description}</p>
+            <h4 class="priceFilter">${item.price}</h4>
+            <button class="btn btn-primary" data-add value="${index}">Add To Cart</button>
+        </div>
+        `;
+    }).join('');
+
+    if (searchInput) {
+        searchInput.addEventListener('input', updateProducts);
     }
-});
 
-console.log(localStorage.getItem('product'));
+    if (typeFilter) {
+        typeFilter.addEventListener('change', updateProducts);
+    }
 
-// Product Sorting and Filtering
-function filterProducts(products) {
-    let searchItems = searchInput.value;
-    let productPrice = priceFilter.value;
+    if (mainProduct) {
+        mainProduct.addEventListener('click', function (event) {
+            if (event.target.hasAttribute('data-add')) {
+                add(event.target.value);
+            }
+        });
+    }
+}
 
-    priceFilter.textContent = '${[productPrice]}';
-
-    // products.forEach(function(product => {
-    //     let title = product.querySelector('.mainProduct').textContent
-};
+initialize();
